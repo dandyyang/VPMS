700a6185849d578b2ac65d08711440ff45fbbb96
klutzdon <padon_huang@foxmail.com>
2016-06-04 16:42:54 +0800

    提交清理后的数据库备份和系统程式

--- Trunk/WindowUI/LoginForm.Designer.cs
+++ Trunk/WindowUI/LoginForm.Designer.cs
@@ -30,48 +30,21 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
-            this.txtUserName = new System.Windows.Forms.TextBox();
-            this.txtPwd = new System.Windows.Forms.TextBox();
-            this.cbxSkin = new System.Windows.Forms.ComboBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.timer2 = new System.Windows.Forms.Timer(this.components);
-            this.btnResert = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbxSkin = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.btnLogin = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.btnResert)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.btnLogin)).BeginInit();
+            this.btnResert = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnLogin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnResert)).BeginInit();
             this.SuspendLayout();
-            // 
-            // txtUserName
-            // 
-            this.txtUserName.BackColor = System.Drawing.SystemColors.Window;
-            this.txtUserName.Location = new System.Drawing.Point(190, 88);
-            this.txtUserName.Name = "txtUserName";
-            this.txtUserName.Size = new System.Drawing.Size(150, 21);
-            this.txtUserName.TabIndex = 0;
-            this.txtUserName.Leave += new System.EventHandler(this.txtUserName_Leave);
-            // 
-            // txtPwd
-            // 
-            this.txtPwd.Location = new System.Drawing.Point(190, 120);
-            this.txtPwd.Name = "txtPwd";
-            this.txtPwd.PasswordChar = '*';
-            this.txtPwd.Size = new System.Drawing.Size(150, 21);
-            this.txtPwd.TabIndex = 1;
-            this.txtPwd.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtPwd_KeyDown);
-            // 
-            // cbxSkin
-            // 
-            this.cbxSkin.FormattingEnabled = true;
-            this.cbxSkin.Location = new System.Drawing.Point(190, 152);
-            this.cbxSkin.Name = "cbxSkin";
-            this.cbxSkin.Size = new System.Drawing.Size(150, 20);
-            this.cbxSkin.TabIndex = 7;
-            this.cbxSkin.SelectedValueChanged += new System.EventHandler(this.cbxSkin_SelectedValueChanged);
             // 
             // timer1
             // 
@@ -82,71 +55,104 @@
             this.timer2.Interval = 2;
             this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
             // 
-            // btnResert
+            // pictureBox1
             // 
-            this.btnResert.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnResert.Image = global::WindowUI.Properties.Resources.bReset;
-            this.btnResert.Location = new System.Drawing.Point(254, 200);
-            this.btnResert.Name = "btnResert";
-            this.btnResert.Size = new System.Drawing.Size(86, 25);
-            this.btnResert.TabIndex = 9;
-            this.btnResert.TabStop = false;
-            this.btnResert.Click += new System.EventHandler(this.btnResert_Click);
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(491, 200);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(51, 50);
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.BackColor = System.Drawing.SystemColors.Window;
+            this.txtUserName.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtUserName.Location = new System.Drawing.Point(244, 56);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(150, 26);
+            this.txtUserName.TabIndex = 16;
+            this.txtUserName.Leave += new System.EventHandler(this.txtUserName_Leave);
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtPwd.Location = new System.Drawing.Point(244, 89);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.PasswordChar = '*';
+            this.txtPwd.Size = new System.Drawing.Size(150, 26);
+            this.txtPwd.TabIndex = 17;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label3.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label3.Location = new System.Drawing.Point(150, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 16);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "样 式：";
+            // 
+            // cbxSkin
+            // 
+            this.cbxSkin.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.cbxSkin.FormattingEnabled = true;
+            this.cbxSkin.Location = new System.Drawing.Point(244, 123);
+            this.cbxSkin.Name = "cbxSkin";
+            this.cbxSkin.Size = new System.Drawing.Size(150, 24);
+            this.cbxSkin.TabIndex = 18;
+            this.cbxSkin.SelectedValueChanged += new System.EventHandler(this.cbxSkin_SelectedValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label2.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label2.Location = new System.Drawing.Point(150, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 16);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "密 码：";
             // 
             // btnLogin
             // 
             this.btnLogin.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnLogin.Image = global::WindowUI.Properties.Resources.bLogin;
-            this.btnLogin.Location = new System.Drawing.Point(139, 200);
+            this.btnLogin.Location = new System.Drawing.Point(185, 171);
             this.btnLogin.Name = "btnLogin";
             this.btnLogin.Size = new System.Drawing.Size(86, 25);
-            this.btnLogin.TabIndex = 8;
+            this.btnLogin.TabIndex = 19;
             this.btnLogin.TabStop = false;
             this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(240)))), ((int)(((byte)(253)))));
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(131)))), ((int)(((byte)(176)))));
-            this.label1.Location = new System.Drawing.Point(137, 90);
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label1.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label1.Location = new System.Drawing.Point(150, 61);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(47, 12);
-            this.label1.TabIndex = 11;
+            this.label1.Size = new System.Drawing.Size(64, 16);
+            this.label1.TabIndex = 21;
             this.label1.Text = "帐 号：";
             // 
-            // label2
+            // btnResert
             // 
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(240)))), ((int)(((byte)(253)))));
-            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(131)))), ((int)(((byte)(176)))));
-            this.label2.Location = new System.Drawing.Point(137, 123);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(47, 12);
-            this.label2.TabIndex = 12;
-            this.label2.Text = "密 码：";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(240)))), ((int)(((byte)(253)))));
-            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(131)))), ((int)(((byte)(176)))));
-            this.label3.Location = new System.Drawing.Point(137, 156);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(47, 12);
-            this.label3.TabIndex = 13;
-            this.label3.Text = "样 式：";
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(486, 193);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
-            this.pictureBox1.TabIndex = 15;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            this.btnResert.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnResert.Image = global::WindowUI.Properties.Resources.bReset;
+            this.btnResert.Location = new System.Drawing.Point(300, 171);
+            this.btnResert.Name = "btnResert";
+            this.btnResert.Size = new System.Drawing.Size(86, 25);
+            this.btnResert.TabIndex = 20;
+            this.btnResert.TabStop = false;
+            this.btnResert.Click += new System.EventHandler(this.btnResert_Click);
             // 
             // LoginForm
             // 
@@ -154,31 +160,31 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::WindowUI.Properties.Resources.PNGbg;
             this.ClientSize = new System.Drawing.Size(545, 252);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.txtPwd);
             this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbxSkin);
             this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnResert);
-            this.Controls.Add(this.btnLogin);
-            this.Controls.Add(this.cbxSkin);
-            this.Controls.Add(this.txtPwd);
-            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "LoginForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.TabText = "登陆窗口";
-            this.Text = "登陆窗口";
+            this.TabText = "登录窗口";
+            this.Text = "登录窗口";
             this.TransparencyKey = System.Drawing.SystemColors.Control;
             this.Load += new System.EventHandler(this.LoginForm_Load);
             this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.LoginForm_MouseUp);
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.LoginForm_MouseDown);
             this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.LoginForm_MouseMove);
-            ((System.ComponentModel.ISupportInitialize)(this.btnResert)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.btnLogin)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnLogin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnResert)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -186,16 +192,16 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox txtUserName;
-        private System.Windows.Forms.TextBox txtPwd;
-        private System.Windows.Forms.ComboBox cbxSkin;
-        private System.Windows.Forms.PictureBox btnLogin;
-        private System.Windows.Forms.PictureBox btnResert;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Timer timer2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbxSkin;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox btnLogin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox btnResert;
     }
 }
